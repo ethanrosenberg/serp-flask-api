2dacb3e4659c70f52178b48c143e25c4c131d90a
Ethan Rosenberg <ethan@ethanrosenberg.com>
2020-04-22 00:39:44 -0700

    Added conditional statements and request type to switch between fuzzywuzzy matching methods.

--- application.py
+++ application.py
@@ -17,11 +17,19 @@ def hello():
     client_json = json.dumps(request.json)
     client_data = json.loads(client_json)
 
+    if client_data["type"] == "SimpleRatio":
+        return {"type": client_data["type"], "response": str(fuzz.ratio(client_data['keyword'], client_data['text'])) }
+    elif client_data["type"] == "PartialRatio":
+        return {"type": client_data["type"], "response": str(fuzz.partial_ratio(client_data['keyword'], client_data['text'])) }
+    elif client_data["type"] == "TokenSortRatio":
+        return {"type": client_data["type"], "response": str(fuzz.token_sort_ratio(client_data['keyword'], client_data['text'])) }
+    elif client_data["type"] == "TokenSetRatio":
+        return {"type": client_data["type"], "response": str(fuzz.token_set_ratio(client_data['keyword'], client_data['text'])) }
+    else:
+        #return str(fuzz.token_set_ratio(client_data['keyword'], client_data['text']))
+        return {"type": client_data["type"], "response": str(fuzz.token_set_ratio(client_data['keyword'], client_data['text'])) }
     #fuzz.token_set_ratio("fuzzy was a bear", "fuzzy fuzzy was a bear")
-
-    return str(fuzz.token_set_ratio(client_data['keyword'], client_data['description']))
-
-
+    #return str(fuzz.token_set_ratio(client_data['keyword'], client_data['text']))
 
 if __name__ == "__main__":
     application.debug = True
